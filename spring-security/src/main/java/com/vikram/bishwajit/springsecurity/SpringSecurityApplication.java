b8256bf75a70ec4ee3a353cfdb81a0f8339958c8
bishwajit01 <bishwajit.vikram@gmail.com>
2020-04-15 09:32:32 +0530

    Spring Security Basic COnfiguration with maven dependencies.

---
+++ spring-security/src/main/java/com/vikram/bishwajit/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,18 @@
+package com.vikram.bishwajit.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Bishwajit.
+ *
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
