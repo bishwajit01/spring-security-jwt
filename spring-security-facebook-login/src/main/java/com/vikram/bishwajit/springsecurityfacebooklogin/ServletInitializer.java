e25671b902c448a3e2b4b6a1ade49e5a87affc18
bishwajit01 <bishwajit.vikram@gmail.com>
2020-04-27 19:10:47 +0530

    Added Spring Boot Securities Login with Github and Facebook using oAuth.

---
+++ spring-security-facebook-login/src/main/java/com/vikram/bishwajit/springsecurityfacebooklogin/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vikram.bishwajit.springsecurityfacebooklogin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringSecurityFacebookLoginApplication.class);
+	}
+
+}
